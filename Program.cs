06c8fa54b752dbcc14be978e0f6e5b264fd7cd27
tnkcoll <theodore.collins2023@outlook.com>
2024-06-12 14:41:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignement6._4_Rotate_2D_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ints = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            int length = ints.GetLength(0);
+
+            Console.WriteLine("Original Matrix:");
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Console.Write($"| {ints[i, j]} ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Rotated Matrix:");
+            for (int j = 0; j < length; j++)
+            {
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    Console.Write($"| {ints[i, j]} ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+        }
+    }
+}
